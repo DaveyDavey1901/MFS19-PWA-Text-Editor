5541255558c00c79aa62ab65eba9fba625a5bd94
David Davies <David_Davies@mail.com>
2022-04-04 21:57:09 +0100

    altered json files to try and resolve issue with heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "JATE",
+        title: "jate",
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
