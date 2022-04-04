bbf73e3e89408571c1fa5b7e676cf27768f3c0a3
David Davies <David_Davies@mail.com>
2022-04-04 22:43:28 +0100

    altered json added heroku pre build

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,23 +16,23 @@ const initdb = async () =>
 export const putDb = async (id, content) => {
   console.log("PUT to the db");
   const jateDb = await openDB("jate", 1);
-  const txt = jateDb.transaction("jate", "readwrite");
-  const store = txt.objectStore("jate");
-  const req = store.put({ id: 1, text: content });
-  const res = await req;
-  console.log("Data has been saved to the db!", res);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, text: content });
+  const result = await request;
+  console.log("Data has been saved to the db!", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the db");
   const jateDb = await openDB("jate", 1);
-  const txt = jateDb.transaction("jate", "readonly");
-  const store = txt.objectStore("jate");
-  const req = store.get(1);
-  const res = await req;
-  console.log("result.value", res);
-  return res;
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  console.log("result.value", result);
+  return result;
 };
 
 initdb();
